5cf51c5b9fca378951ed0547b85e28309ccd5e8b
Jameelah <jemilat.rotimi@gmail.com>
2023-10-18 12:33:06 +0100

    commit

--- src/Hamburger.jsx
+++ src/Hamburger.jsx
@@ -1,35 +1,56 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import './styles/hamburger.css'
-
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./styles/hamburger.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 function Hamburger() {
-    const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-        console.log('open')
-    }
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    console.log("open");
+  };
 
   return (
     <>
-        <div className={`hamburger-menu ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
-           <button className="hamburger" >
-              <span className="icon"></span>
-              <span className="icon"></span>
-              <span className="icon"></span>
-           </button>
+      <div
+        className={`hamburger-menu ${isOpen ? "open" : ""}`}
+        onClick={toggleMenu}
+      >
+        <button className="hamburger" >
+        {isOpen ? (
+          <FontAwesomeIcon icon={faBars} size={'2x'} color={'white'}/>
+        ) : (
+          <FontAwesomeIcon icon={faXmark} size={'2x'} color={'white'}/>
+        )}
+        </button>
 
-            <ul className={`Links ${isOpen ? '' : 'icon'}`} >
-              <li className="nav-item"><Link to={'/'} className="navlink"> Home </Link></li>
-              <li className="nav-item"> <Link to={'/about'} className="navlink"> About </Link></li>
-              <li className="nav-item"><Link to={'/project'} className="navlink"> Project </Link></li>
-              <li className="nav-item"> <Link to={'/contact'} className="navlink"> Contact </Link></li>
-            </ul>
-        </div>
+        <ul className={`Links ${isOpen ? "icon" : ""}`}>
+          <li className="nav-item">
+            <Link to={"/"} className="navlink">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/about"} className="navlink">
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/project"} className="navlink">
+              Project
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/contact"} className="navlink">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
     </>
-  )
+  );
 }
 
-export default Hamburger
+export default Hamburger;
